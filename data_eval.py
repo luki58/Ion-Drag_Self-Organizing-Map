bd3ee7fb9d1b6e10640f441b1a09a22a0d7b04b7
Lukas <wimmerlukas@gmx.net>
2024-11-12 14:12:05 +0100

    Update data_eval.py

--- data_eval.py
+++ data_eval.py
@@ -2,15 +2,14 @@
 """
 Created on Thu Jun 13 18:58:37 2024
 
-@author: Sali
+@author: Luki
 """
 
 import os
 import numpy as np
 import matplotlib.pyplot as plt
 import json
-
-
+from scipy.optimize import curve_fit
 
 #%% v mean plots
 
@@ -25,7 +24,7 @@ for file in file_list:
     dataset.append(json_data)
 
 
-if json_folder.split('/')[2] == '1p5mA':
+if json_folder.split('/')[1] == 'Argon':
     v_mean_pos = np.array(())
     pressure_pos = np.array(())
     v_error_pos = np.array(())
@@ -64,44 +63,40 @@ else:
     v_error_neg = v_error[0::2]*1000
     pressure_neg = pressure[0::2]
 
-colors = ['g','b']
+# Define the fitting model: v = c1 * p**(-1) + c2 * p**(-2) + c3 * p**(-3)
+def inverse_power_model(p, c0, c1, c2, c3):
+    return c1 * p**(-1) + c2 * p**(-2) + c3 * p**(-3)
+
+# Fit model for positive polarity
+popt_pos, pcov_pos = curve_fit(inverse_power_model, pressure_pos, v_mean_pos, sigma=v_error_pos, absolute_sigma=True)
+c0_pos, c1_pos, c2_pos, c3_pos = popt_pos
+
+# Fit model for negative polarity
+popt_neg, pcov_neg = curve_fit(inverse_power_model, pressure_neg, v_mean_neg, sigma=v_error_neg, absolute_sigma=True)
+c0_neg, c1_neg, c2_neg, c3_neg = popt_neg
+
+# Generate a smooth line for plotting the fit
+pressure_range = np.linspace(13, 120, 500)  # Start from 0.1 to avoid division by zero
+fit_pos = inverse_power_model(pressure_range, *popt_pos)
+fit_neg = inverse_power_model(pressure_range, *popt_neg)
+
+# Plotting the results
 plt.figure(dpi=500)
-plt.errorbar(pressure_neg, v_mean_neg, v_error_neg, fmt='d', color='r', linewidth=1, markersize=3, capsize=2, mfc='w', ecolor='g', label='negative polarity')
-plt.errorbar(pressure_pos, v_mean_pos, v_error_pos, fmt='o', color='r', linewidth=1, markersize=3, capsize=2, mfc='w', ecolor='b', label='positive polarity')
+plt.errorbar(pressure_neg, v_mean_neg, yerr=v_error_neg, fmt='d', color='r', linewidth=1, markersize=3, capsize=2, mfc='w', ecolor='g', label='Negative Polarity')
+plt.errorbar(pressure_pos, v_mean_pos, yerr=v_error_pos, fmt='o', color='r', linewidth=1, markersize=3, capsize=2, mfc='w', ecolor='b', label='Positive Polarity')
+
+# Plot the nonlinear fit lines
+plt.plot(pressure_range, fit_neg, 'g--', label=f'Negative Fit', linewidth=.7)
+plt.plot(pressure_range, fit_pos, 'b--', label=f'Positive Fit', linewidth=.7)
+
+print("Polynomials (POS): c0 = " + str(c0_pos) + ", c1 = " + str(c1_pos) + ", c2 = "+ str(c2_pos) + ", c3 = " + str(c3_pos))
+print("Polynomials (NEG): c0 = " + str(c0_neg) + ", c1 = " + str(c1_neg) + ", c2 = "+ str(c2_neg) + ", c3 = " + str(c3_pos))
+
+# Labels, title, and legend
 plt.xlabel('Pressure [Pa]')
 plt.ylabel('$v_{mean}$ [mm/s]')
-plt.grid(color='gray', linestyle='-', linewidth=.2)
+plt.grid(color='gray', linestyle='-', linewidth=0.2)
 plt.title(str(json_folder.split('/')[1]) + ' ' + str(json_folder.split('/')[2]))
 plt.legend(loc='upper right')
-plt.xlim(0,130)
+plt.xlim(0, 130)
 plt.show()
-
-#%% alphas
-
-#Neon
-pressure = np.array([120, 100, 90, 70, 60, 50, 40])
-alpha_neg = np.array([0.0133, 0.0156, 0.0283, 0.029, 0.032, 0.0102, 0.0059])
-alpha_pos = np.array([0.0031, 0.0079, 0.0179, 0.0245, 0.0251, 0.0044, 0.001]) 
-
-#Argon
-# pressure = np.array([120,110,100,90,80,70,60,50,40,30,23,15])
-# alpha_neg = np.array([0.0022, 0.0108, 0.001, 0.0036, 0.006, 0.0038, 0.0041, 0.0045, 0.0104, 0.0191, 0.0066, 0.0132])
-# alpha_pos = np.array([0.0011, 0.0061, 0.0018, 0.001, 0.001, 0.0047, 0.0035, 0, 0.0097, 0.0136, 0.0067, 0.0083])
-
-
-plt.figure(dpi=500)
-
-plt.plot(pressure, alpha_neg, 'x', c='blue')
-plt.plot(pressure, alpha_neg, '--', c='blue', linewidth=0.5, label='negative polarity')
-
-plt.plot(pressure, alpha_pos, 'x', c='red')
-plt.plot(pressure, alpha_pos, '--', c='red', linewidth=0.5, label='positive polarity')
-
-plt.ylabel('alpha')
-plt.xlabel('pressure [Pa]')
-plt.title(str(json_folder.split('/')[1]) + ' ' + str(json_folder.split('/')[2]))
-plt.legend(loc='upper right')
-plt.show()
-
-
-
